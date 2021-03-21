fc5f539c34899c897956291b5f796826a6d0412a
yukou-isshiki <yff24517@nifty.com>
2021-03-21 21:58:06 +0900

    一部修正

--- root_analysis.py
+++ root_analysis.py
@@ -10,22 +10,25 @@ def root_calc(start, end, station_dict, filename):
     lines = f.readlines()
     f.close()
     univ = []
+    weights = {}
     for line in lines:
         dat = line.split(",")
         dat[0] = station_dict[dat[0]]
         dat[1] = station_dict[dat[1]]
-        dat[2] = float(dat[2])
-        edge = tuple(dat[0:3])
+        edge = tuple(dat[0:2])
         univ.append(edge)
+        weights[(dat[0],dat[1])] = float(dat[2])
         del dat
         del edge
         gc.collect()
-    w = gs.set_universe(univ)
+    gs.set_universe(univ)
     start = station_dict[start]
     end = station_dict[end]
+    del station_dict
+    gc.collect()
     paths = gs.paths(start, end)
-    max_path = next(paths.max_iter())
-    return max_path, univ
+    max_path = next(paths.max_iter(weights))
+    return max_path, weights
 
 def root_print(path, search, end, station_dict, root_list):
     another = ""
@@ -63,10 +66,8 @@ if __name__ == '__main__':
     end_number = station_dict[end]
     root_list = []
     distance = Decimal("0.0")
-    for edge in all_edge:
-        for path_edge in path:
-            if edge[0] == path_edge[0] and edge[1] == path_edge[1]:
-                distance += Decimal(str(edge[2]))
+    for edge in path:
+        distance += Decimal(str(all_edge[edge]))
     creat_list = root_print(path, start_number, end_number, station_dict, root_list)
     creat_list.append(end)
     most_longest_root = "→".join(creat_list)
